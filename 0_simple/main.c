afd0199d78d36b63523f234eba7fd443a76d7523
Christopher Klix <christopher.klix@googlemail.com>
2022-10-30 17:17:40 +0100

    update

--- 0_simple/main.c
+++ 0_simple/main.c
@@ -8,7 +8,33 @@ short get_short(char[], short, short);
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    // Declare & Init constants
+    const short MIN = 1;
+    const short MAX = 49;
+
+
+    // Array length
+    const int ARR_LEN = 6;
+
+    // Declare array of 6 int
+    // Initialize array
+    short numbers[ARR_LEN] = { 0 };
+
+    // Assign user input to array
+    for (int i = 0; i < ARR_LEN; i++)
+    {
+        numbers[i] = get_short("Enter a number: ", MIN, MAX);
+    }
+
+
+    // Print array to console
+    for (int i = 0; i < ARR_LEN; i++)
+    {
+        printf("%3hd", numbers[i]);
+    }
+    
+    printf("\n");
+    
     return 0;
 }
 
@@ -25,7 +51,7 @@ short get_short(char txt[], short min, short max)
     do
     {
         // get user input
-        printf("%s: ", txt);
+        printf("%s", txt);
         ch = '\0';
         ret_val = scanf("%hd%c", &value, &ch);
 
