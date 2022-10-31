37095d07649c329efa12001701b769c4eea8c8f7
Christopher Klix <christopher.klix@googlemail.com>
2022-10-31 12:01:47 +0100

    updated simple

--- 0_simple/main.c
+++ 0_simple/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
 
 
+void print_array(short[], short);
+bool number_is_unique(short, short[], short);
 short get_short(char[], short, short);
 
 
@@ -14,30 +17,92 @@ int main(int argc, char const *argv[])
 
 
     // Array length
-    const int ARR_LEN = 6;
+    const int NUMBER_COUNT = 6;
 
     // Declare array of 6 int
     // Initialize array
-    short numbers[ARR_LEN] = { 0 };
+    short user_numbers[NUMBER_COUNT] = { 0 };
+    short winning_numbers[NUMBER_COUNT] = { 0 };
 
     // Assign user input to array
-    for (int i = 0; i < ARR_LEN; i++)
+    for (int i = 0; i < NUMBER_COUNT; i++)
     {
-        numbers[i] = get_short("Enter a number: ", MIN, MAX);
+        user_numbers[i] = get_short("Enter a number: ", MIN, MAX);
     }
-
 
     // Print array to console
-    for (int i = 0; i < ARR_LEN; i++)
+    printf("Your numbers are:\n");
+    print_array(user_numbers, NUMBER_COUNT);
+
+    // pick numbers
+    srand(time(NULL));
+
+    // Assign random numbers to array
+    for (int i = 0; i < NUMBER_COUNT; i++)
     {
-        printf("%3hd", numbers[i]);
+        // Holding our generated random number
+        short random_number;
+
+        // Generate a random number and
+        // repeat if the random number
+        // is not unique in the winning_numbers
+        do
+        {
+            // generating a pseudo-random number with rand()
+            // by using modulo we map
+            //      0 - MAX_RANGE       MAX_RANGE: provided by stdlib.h
+            //      to
+            //      0 - (MAX-1)         0 - 48
+            // 
+            // we then map
+            //      0 - (MAX-1)         0 - 48
+            //      to
+            //      1 - MAX             1 - 49
+            // by adding 1
+            random_number = (rand() % MAX) + 1;
+        } while (!number_is_unique(random_number, winning_numbers, NUMBER_COUNT));
+        
+        // Now that we found a unique random number
+        // we assign it to the winning_numbers array
+        winning_numbers[i] = random_number;
     }
-    
-    printf("\n");
+
+    // Print array to console
+    printf("The winning numbers are:\n");
+    print_array(winning_numbers, NUMBER_COUNT);
     
     return 0;
 }
 
+
+void print_array(short array[], short arr_len)
+{
+    for (int i = 0; i < arr_len; i++)
+    {
+        printf("%3hd", array[i]);
+    }
+
+    printf("\n");
+
+    return;
+}
+
+bool number_is_unique(short number, short array[], short arr_len)
+{
+    // Iterate over array and see if there are matching numbers
+    // If so, then return with false
+    for (int i = 0; i < arr_len; i++)
+    {
+        if (number == array[i])
+        {
+            return false;
+        }
+    }
+
+    // Since the for-loop completed and found no
+    // matching numbers, we return true
+    return true;
+}
 
 short get_short(char txt[], short min, short max)
 {
